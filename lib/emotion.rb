b416a109bb77d906d76afb2f934fafa711aecb0f
Ivan Zarea <zarea.ion@gmail.com>
2013-10-21 10:28:46 +0300

    Major refactoring

--- lib/emotion.rb
+++ lib/emotion.rb
@@ -1,6 +1,6 @@
 require_relative "emotion/version"
 
-methods = %w(ohgodwhy awyeah areyoukiddingme badass dealwithit facepalm foreveralone fuckyeah iseewhatyoudidthere itsatrap! lolwut megusta ohcrap sweetjesus dolan pls wat wtf yey)
+methods = %w(ohgodwhy awyeah areyoukiddingme badass dealwithit facepalm foreveralone fuckyeah iseewhatyoudidthere itsatrap! lolwut megusta ohcrap sweetjesus dolan pls wat wtf yey gooby)
 
 methods.each do |method_name|
   BasicObject.class_eval <<-STR
